bd17bb53d155958e5e8568d016268ec1e16e2f99
nickNW <59003980+nickNW@users.noreply.github.com>
2022-04-12 13:22:42 -0400

    made changes with TA

--- main.c
+++ main.c
@@ -12,37 +12,55 @@
 typedef struct _rwlock_t {
     sem_t writelock;
     sem_t lock;
+    sem_t resource;
     int readers;
+    int writers;
 } rwlock_t;
 
 void rwlock_init(rwlock_t *lock) {
     lock->readers = 0;
-    sem_init(&lock->lock, 1, 100); 
-    sem_init(&lock->writelock, 1,100); 
+    lock->writers = 0;
+    sem_init(&lock->lock, 0, 1); 
+    sem_init(&lock->writelock, 0, 1); 
+    sem_init(&lock->resource,0, 1); 
 }
 
 void rwlock_acquire_readlock(rwlock_t *lock) {
     sem_wait(&lock->lock);
     lock->readers++;
-    if (lock->readers == 1)
-	sem_wait(&lock->writelock);
+    
+    if (lock->readers == 0 || lock->writers > 0){
+	    sem_post(&lock->lock);
+        sem_wait(&lock->resource); 
+        sem_wait(&lock->lock);
+        //printf("%d Wait writelock " ,lock->readers);
+    }
     sem_post(&lock->lock);
 }
 
 void rwlock_release_readlock(rwlock_t *lock) {
     sem_wait(&lock->lock);
     lock->readers--;
-    if (lock->readers == 0)
-	sem_post(&lock->writelock);
+    //printf("%d Read count -- " ,lock->readers);
+    if (lock->readers == 0 ){
+	    sem_post(&lock->resource);
+        //printf("%d post writelock " ,lock->readers);
+    }
     sem_post(&lock->lock);
 }
 
 void rwlock_acquire_writelock(rwlock_t *lock) {
-    sem_wait(&lock->writelock);
+    sem_wait(&lock->writelock);  
+    lock->writers++;
+    sem_post(&lock->writelock); 
+    sem_wait(&lock->resource);
 }
 
 void rwlock_release_writelock(rwlock_t *lock) {
+    sem_wait(&lock->writelock);
+     lock->writers--;
     sem_post(&lock->writelock);
+    sem_post(&lock->resource);
 }
 
 int read_loops;
@@ -53,9 +71,9 @@ rwlock_t mutex;
 
 void *readThread(void *arg) {
     int local = 0;
-    
-	rwlock_acquire_readlock(&mutex);
     printf("Create reader \n");
+	rwlock_acquire_readlock(&mutex);
+
     int x=0, T;      
     T = rand()%10000;   
     for(int i = 0; i < T; i++)   
@@ -70,9 +88,9 @@ void *readThread(void *arg) {
 }
 
 void *writeThread(void *arg) {
-    
-	rwlock_acquire_writelock(&mutex);
     printf("Create Write\n");   
+	rwlock_acquire_writelock(&mutex);
+      
     int x=0, T;      
     T = rand()%10000;   
     for(int i = 0; i < T; i++)   
@@ -99,7 +117,7 @@ int main()
 
     //initialize lock
    //rwlock_init(&mutex);
-     rwlock_init(&mutex); 
+     
     //for threads
     pthread_t thread;
 
@@ -114,7 +132,7 @@ int main()
             {
 
                 //if read create a thread to run the readThread
-                error = pthread_create(&thread, NULL, (void *)readThread, NULL);
+                error = pthread_create(&thread, NULL, (void *)readThread, (void *)&mutex);
                 if (error != 0)
                 {
                     printf("Can't create thread.\n");
@@ -126,7 +144,7 @@ int main()
             {
 
                 //if write create a thread to run the writeThread
-                error = pthread_create(&thread, NULL, (void *)writeThread, NULL);
+                error = pthread_create(&thread, NULL, (void *)writeThread, (void *)&mutex);
                 if (error != 0)
                 {
                     printf("Can't create thread.\n");
